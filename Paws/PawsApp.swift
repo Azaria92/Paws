93e66097bac7065fa41953500be7f97f76c91262
azaria <wora.azaria@gmail.com>
2022-10-26 22:37:13 +0000

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Wora Onanga Azaria Amour Benny on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
